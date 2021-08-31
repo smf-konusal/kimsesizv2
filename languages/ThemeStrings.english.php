a0c865795d02c364daec3276fbdaa607645daca2
CeeMoo <qdale.123@gmail.com>
2021-08-31 19:20:27 +0300

    Initial commit

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,32 @@
+<?php
+
+$txt['themecopyright'] = ' <strong>Bese Theme By <a href="https://smf.konusal.com/" target="_blank">Cee</a></strong>';
+
+
+$txt['bese_color_default'] = 'bese_color_default';
+$txt['bese_color_primary'] = 'bese_color_primary';
+$txt['bese_color_secondary'] = 'bese_color_secondary';
+$txt['bese_default_mode'] = 'bese_default_mode';
+
+$txt['bese_color_defaulte'] = 'Default is: %color%';
+
+
+$txt['bese_default_mode'] = 'Default Mode';
+$txt['bese_dark_mode'] = 'Dark Mode';
+$txt['bese_light_mode'] = 'Light Mode';
+$txt['bese_theme_settings'] = 'Kimsesiz Theme Settings';
+
+$txt['bese_allow_user_modes'] = 'bese_allow_user_modes';
+$txt['bese_allow_color_select'] = 'bese_allow_color_select';
+
+$txt['default_bese_color'] = '#a7dcbd';
+$txt['default_bese_primary'] = '#7557ac';
+$txt['default_bese_secondary'] = '#444361';
+
+
+$txt['bese_color_kimsesiz_ex_title'] = 'bese_color_kimsesiz_ex_title';
+$txt['bese_color_kimsesiz_ex'] = '#E95E19';
+
+$txt['bese_color_kimsesiz_title'] = 'bese_color_kimsesiz_color_title';
+$txt['bese_color_kimsesiz'] = '#0d0d0d';
+?>
